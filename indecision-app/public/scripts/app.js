334a98e179367be380c1298863c7b67090bc829d
alexb31 <alexandrebornstein@gmail.com>
2018-03-27 01:22:28 +0200

    Played with JSX

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -1,10 +1,46 @@
 "use strict";
 
+var appObject = {
+    title: "test title",
+    subtitle: "test subtitle"
+};
+
 var template = React.createElement(
-  "p",
-  { id: "test" },
-  "YEAH BABY !!"
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        { id: "test" },
+        appObject.title
+    ),
+    React.createElement(
+        "p",
+        null,
+        appObject.subtitle
+    )
+);
+
+var template2 = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "Alex B"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Age: 23"
+    ),
+    React.createElement(
+        "p",
+        null,
+        "Location: Paris"
+    )
 );
 var appRoot = document.getElementById('app');
+var appRoot2 = document.getElementById('app2');
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot2);
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -1,4 +1,22 @@
-var template = <p id="test">YEAH BABY !!</p>;
+var appObject = {
+    title : "test title",
+    subtitle : "test subtitle"
+};
+
+var template = 
+<div>
+    <h1 id="test">{appObject.title}</h1>
+    <p>{appObject.subtitle}</p>
+</div>;
+
+var template2 = 
+<div>
+    <h1>Alex B</h1>
+    <p>Age: 23</p>
+    <p>Location: Paris</p>
+</div>
 var appRoot = document.getElementById('app');
+var appRoot2 = document.getElementById('app2')
 
 ReactDOM.render(template, appRoot);
+ReactDOM.render(template2, appRoot2)
