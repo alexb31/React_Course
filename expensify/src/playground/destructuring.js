523f4650ece640bb0cdfdb76a942d433b37c55e7
alexb31 <alexandrebornstein@gmail.com>
2018-04-22 21:16:48 +0200

    Destructuring Arrays

--- expensify/src/playground/destructuring.js
+++ expensify/src/playground/destructuring.js
@@ -17,14 +17,26 @@
 //     console.log(`It's ${temperature} in ${city}`);
 // }
 
-const book = {
-    title: 'Ego is the Enemy',
-    author: 'Ryan Holiday',
-    publisher: {
-        name: 'Penguin'
-    }
-};
+// const book = {
+//     title: 'Ego is the Enemy',
+//     author: 'Ryan Holiday',
+//     publisher: {
+//         name: 'Penguin'
+//     }
+// };
 
-const {name: publisherName = "Error"} = book.publisher; 
+// const {name: publisherName = "Error"} = book.publisher; 
 
-console.log(publisherName);
+// console.log(publisherName);
+
+const adress = ['1299 Rue de la Paix', 'Paris', 'France', '75100'];
+
+const [city, state] =  adress;
+
+console.log(`You are in ${city} ${state}`);
+
+const item = ['Coffee (hot)', '$2.00', '$2.50', '$2.75'];
+
+const [coffee, , price] = item;
+
+console.log(`A medium ${coffee} costs ${price}`);
