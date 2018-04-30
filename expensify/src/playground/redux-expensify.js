e37a575f1e96424a0f6273ed51aaf0ffa112e9ef
Alex <alexandre.bornstein@sublimeskinz.com>
2018-04-30 18:10:11 +0200

    Spreading Object

--- expensify/src/playground/redux-expensify.js
+++ expensify/src/playground/redux-expensify.js
@@ -15,6 +15,7 @@ const addExpense = (
         id: uuid(),
         description,
         note, 
+        amount,
         createdAt
     }
 });
@@ -25,7 +26,17 @@ const removeExpense = ({ id } =  {}) => ({
 });
 
 // EDIT_EXPENSE
-// SET_TEST_FILTER
+const editExpense = (id, updates) => ({
+    type: 'EDIT_EXPENSE',
+    id,
+    updates
+});
+
+// SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
 
 const expensesReducerDefaultState = [];
 
@@ -38,6 +49,17 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
             ];
         case 'REMOVE_EXPENSE':
             return state.filter(({ id }) => id !== action.id);
+        case 'EDIT_EXPENSE':
+            return state.map((expense) => {
+                if(expense.id === action.id) {
+                    return {
+                        ...expense,
+                        ...action.updates
+                    };
+                } else {
+                    return expense;
+                };
+            });
         default:
         return state;
     }
@@ -46,7 +68,7 @@ const expensesReducer = (state = expensesReducerDefaultState, action) => {
 // Filters Reducer
 
 const filterReducerDefaultState = {
-    text: '',
+    text: 'Default',
     sortBy: 'date',
     startDate: undefined,
     endDate: undefined
@@ -54,6 +76,11 @@ const filterReducerDefaultState = {
 
 const filtersReducer = (state = filterReducerDefaultState, action) => {
     switch (action.type) {
+        case 'SET_TEXT_FILTER':
+            return {
+                ...state,
+                text: action.text
+            }
         default:
         return state;
     }
@@ -73,9 +100,13 @@ store.subscribe(() => {
 });
 
 const expenseOne = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
-const expenseTwo = store.dispatch(addExpense({ description: 'Akira', amount: 1000 }));
+const expenseTwo = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
 
 store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500}));
+
+store.dispatch(setTextFilter('baba'));
+store.dispatch(setTextFilter());
 
 const demoState = {
     expenses: [{
